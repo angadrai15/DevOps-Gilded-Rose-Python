c55ce26c3258ba39af685ead93f4d31e34acfb17
angadrai15 <angadrai15@gmail.com>
2025-01-21 14:24:55 +0000

    comment out the entire block of code between 3 dots

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -2,7 +2,7 @@ class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
-
+    ...
     def update_quality(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
@@ -32,3 +32,4 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+    ...
